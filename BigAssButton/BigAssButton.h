3119091cac02c6dc170b5ee113469ab82bb292e3
Ashton Meuser <ashtonmeuser@gmail.com>
2017-11-02 16:10:22 -0700

    Initial Commit

---
+++ BigAssButton/BigAssButton.h
@@ -0,0 +1,19 @@
+//
+//  BigAssButton.h
+//  BigAssButton
+//
+//  Created by Ashton Meuser on 2017-11-02.
+//  Copyright © 2017 Ashton Meuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BigAssButton.
+FOUNDATION_EXPORT double BigAssButtonVersionNumber;
+
+//! Project version string for BigAssButton.
+FOUNDATION_EXPORT const unsigned char BigAssButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigAssButton/PublicHeader.h>
+
+
